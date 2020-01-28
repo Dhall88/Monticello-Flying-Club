c70ef41f7621b9d46c7bcb0c38960804380adaef
David Hall <davidhall@Davids-MacBook-Pro.local>
2020-01-28 14:36:04 -0500

    rough skeleton complete

---
+++ app.js
@@ -0,0 +1,20 @@
+// function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+//   }
+  
+//   // Close the dropdown menu if the user clicks outside of it
+//   window.onclick = function(event) {
+//     if (!event.target.matches('.dropbtn')) {
+//       var dropdowns = document.getElementsByClassName("dropdown-content");
+//       var i;
+//       for (i = 0; i < dropdowns.length; i++) {
+//         var openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')) {
+//           openDropdown.classList.remove('show');
+//         }
+//       }
+//     }
+//   }
+
+let n =1234;
+console.log(n[1])
